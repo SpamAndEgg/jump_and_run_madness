4ea6849bac3445449b94de70d6a23cd3ffc5c82c
Marlon Fleck <marlonalexander.fleck@gmail.com>
2017-12-02 19:33:09 +0100

    First playable verion with collision check, simple level, enemies and gravity.

--- main.py
+++ main.py
@@ -39,15 +39,57 @@ monster_width_dead = img_monster_dead_size[2]
 monster_height_dead = img_monster_dead_size[3]
 
 
+# Collision check with ground for player and enemy object.
+def collision_check_ground(this_object):
+    this_object.is_on_ground = False
+    for i_ground in level.active_ground:
+        x_collision = False
+        on_ground_before = False
+        y_collision = False
+        # Check if object and this ground collide in x direction.
+        if this_object.x_pos < i_ground[0] + i_ground[2] and this_object.x_pos + this_object.width > i_ground[0]:
+            x_collision = True
+        # Check if object had been on ground before.
+        if this_object.x_pos - this_object.x_change < i_ground[0] + i_ground[2] and this_object.x_pos - \
+                this_object.x_change + this_object.width > i_ground[0]:
+            on_ground_before = True
+        # Check if object and this ground collide in y direction.
+        if this_object.y_pos < i_ground[1] + i_ground[3] and this_object.y_pos + this_object.height > i_ground[1]:
+            y_collision = True
+
+        if x_collision and y_collision:
+            # Check if player hit ground from left or right.
+            if this_object.x_change > 0 and not on_ground_before:
+                this_object.x_pos = i_ground[0] - this_object.width
+                this_object.x_velocity_flight = 0
+                this_object.x_velocity = 0
+            elif this_object.x_change < 0 and not on_ground_before:
+                this_object.x_pos = i_ground[0] + i_ground[2]
+                this_object.x_velocity_flight = 0
+                this_object.x_velocity = 0
+            # Check if player hit ground from top or bottom
+            elif this_object.y_change > 0:
+                # Player is on ground.
+                this_object.y_pos = i_ground[1] - this_object.height
+                this_object.is_on_ground = True
+                this_object.y_velocity = 0
+            elif this_object.y_change < 0:
+                this_object.y_pos = i_ground[1] + i_ground[3]
+
+
 class Player:
     def __init__(self, x_pos, y_pos):
         self.x_pos = x_pos
         self.y_pos = y_pos
+        self.x_change = []
+        self.y_change = []
         self.x_velocity = 0
         self.x_velocity_flight = 0
         self.x_velocity_ground = 0
         self.y_velocity = 0
         self.is_on_ground = False
+        self.width = character_width
+        self.height = character_height
 
     def blit(self):
         x_pos_on_screen = self.x_pos - camera.left_edge
@@ -79,43 +121,100 @@ class Player:
         elif self.y_pos > display_height - character_height:
             # If character goes outsite of bottom of screen he dies.
             display_message('YOU FELL THROUGH THE GROUND, NOW YOU ARE DEAD!!')
+        elif self.x_pos > 6000:
+            display_message('YEEHA, YOU WON THE GAME!!')
 
     def collision_check(self):
-        x_change = self.x_velocity
-        y_change = self.y_velocity
-        self.is_on_ground = False
+        self.x_change = self.x_velocity
+        self.y_change = self.y_velocity
+        #self.is_on_ground = False
         # Iterate over each ground element and check if a collision is detected.
-        for i_ground in level.active_ground:
-            x_collision = False
-            on_ground_before = False
-            y_collision = False
+        collision_check_ground(self)
 
-            if self.x_pos < i_ground[0] + i_ground[2] and self.x_pos + character_width > i_ground[0]:
+
+class Enemy:
+    def __init__(self, x_enemy, y_enemy):
+        self.x_velocity = -7
+        self.x_base_velocity = -7
+        self.y_velocity = 0
+        self.x_change = []
+        self.y_change = []
+        self.x_pos = x_enemy
+        self.last_x_pos = []
+        self.x_pos_on_screen = []
+        self.y_pos = y_enemy
+        self.is_dead = False
+        self.is_on_ground = False
+        self.width = monster_width
+        self.height = monster_height
+
+    def blit(self):
+        if self.is_dead:
+            Game_display.blit(img_monster_dead, (self.x_pos_on_screen, self.y_pos))
+        else:
+            Game_display.blit(img_monster, (self.x_pos_on_screen, self.y_pos))
+
+    def collision_check(self):
+        is_headjump = False
+        is_kill_player = False
+        x_collision = False
+        y_collision = False
+        # COLLISION CHECK PLAYER
+        # Check if there is a collision from any side (top, right, down, left).
+        if not self.is_dead:
+            # Check if x range of monster and character overlap.
+            if player.x_pos < self.x_pos + self.width and player.x_pos + player.width > self.x_pos:
                 x_collision = True
-
-            if self.x_pos-x_change < i_ground[0] + i_ground[2] and self.x_pos-x_change + character_width > i_ground[0]:
-                on_ground_before = True
-
-            if self.y_pos < i_ground[1] + i_ground[3] and self.y_pos + character_height > i_ground[1]:
+            if player.y_pos + player.height > self.y_pos and player.y_pos < self.y_pos + self.height:
                 y_collision = True
-
             if x_collision and y_collision:
-                # Check if player hit ground from left or right.
-                if x_change > 0 and not on_ground_before:
-                    self.x_pos = i_ground[0] - character_width
-                    self.x_velocity_flight = 0
-                elif x_change < 0 and not on_ground_before:
-                    self.x_pos = i_ground[0] + i_ground[2]
-                    self.x_velocity_flight = 0
-                # Check if player hit ground from top or bottom
-                elif y_change > 0:
-                    # Player is on ground.
-                    self.y_pos = i_ground[1] - character_height
-                    self.is_on_ground = True
-                    self.y_velocity = 0
-                elif y_change < 0:
-                    self.y_pos = i_ground[1] + i_ground[3]
+                # Check for head collision (player had been above monster head in last frame).
+                if player.y_pos + player.height - player.y_velocity < self.y_pos:
+                    is_headjump = True
+                else:
+                    is_kill_player = True
 
+        # COLLISION CHECK LEVEL
+        self.x_change = self.x_velocity
+        self.y_change = self.y_velocity
+        collision_check_ground(self)
+
+        # If the hero touches the monster (unless headjump) the hero dies.
+        if is_kill_player:
+            display_message('YOU WERE KILLED BY A HORRIBLE MONSTER!')
+        # If the hero jumped on the monsters head, it dies.
+        if is_headjump:
+            self.is_dead = True
+            self.x_velocity = 0
+            # Even out the hight differences of the two pictures for alive and dead monster.
+            #self.y_pos += monster_height - monster_height_dead
+            self.height = monster_height_dead
+
+    # Update the enemy for the next frame.
+    def update_pos(self):
+        # Update x position.
+        self.x_pos += self.x_velocity
+        # Update y position
+        self.y_velocity += gravity
+        if self.y_velocity > max_fall_velocity:
+            self.y_velocity = max_fall_velocity
+        # Update y position. Therefore first check falling due to gravity.
+        self.y_pos += self.y_velocity
+        # Check collision with player.
+        self.collision_check()
+        # If the monster can't move forward, reverse its running direction.
+        if self.last_x_pos == self.x_pos and not self.is_dead:
+            self.x_base_velocity *= -1
+            self.x_velocity = self.x_base_velocity
+        # Update the last position.
+        self.last_x_pos = self.x_pos
+
+    def draw(self):
+        # Find position on screen.
+        self.x_pos_on_screen = self.x_pos - camera.left_edge
+
+        # Draw the enemy.
+        self.blit()
 
 
 class Camera:
@@ -165,16 +264,20 @@ class Level:
         # Check each ground element if it lies within the camera frame plus max. camera velocity (so each element for
         # the next frame is given out).
 
-        this_ground = []
-        for i_ground in self.ground:
-            # Check, if left edge of floor is not out of the right side of the camera and right edge of floor is not
-            # out of the left side of the camera.
-            left_ok = i_ground[0] + i_ground[2] > camera.left_edge
-            right_ok = i_ground[0] < camera.right_edge
-            if left_ok and right_ok:
-                this_ground.append(i_ground)
+        # this_ground = []
+        # for i_ground in self.ground:
+        #     # Check, if left edge of floor is not out of the right side of the camera and right edge of floor is not
+        #     # out of the left side of the camera.
+        #     left_ok = i_ground[0] + i_ground[2] > camera.left_edge
+        #     right_ok = i_ground[0] < camera.right_edge
+        #     if left_ok and right_ok:
+        #         this_ground.append(i_ground)
+        #
+        # self.active_ground = this_ground
 
-        self.active_ground = this_ground
+        # PROBLEM: Ground elements are only dependant of character vision, enemies are not effected. Therefore the whole
+        # gound is given back as active ground
+        self.active_ground = self.ground
 
     def draw_ground(self):
         # Draw all the active ground on the screen.
@@ -187,65 +290,7 @@ class Level:
             pygame.draw.rect(Game_display, black, this_ground)
 
 
-class Enemy:
-    def __init__(self, x_enemy, y_enemy):
-        self.x_speed = 7
-        self.y_speed = 0
-        self.x_pos = x_enemy
-        self.x_pos_on_screen = []
-        self.y_pos = y_enemy
-        self.is_dead = False
-        self.width = monster_width
-        self.height = monster_height
 
-    def blit(self):
-        if self.is_dead:
-            Game_display.blit(img_monster_dead, (self.x_pos_on_screen, self.y_pos))
-        else:
-            Game_display.blit(img_monster, (self.x_pos_on_screen, self.y_pos))
-
-    def collision_check(self):
-        is_headjump = False
-        is_kill_player = False
-        x_collision = False
-        y_collision = False
-        # Check if there is a collision from any side (top, right, down, left).
-        if not self.is_dead:
-            # Check if x range of monster and character overlap.
-            if player.x_pos < self.x_pos + self.width and player.x_pos + character_width > self.x_pos:
-                x_collision = True
-            if player.y_pos + character_height > self.y_pos and player.y_pos < self.y_pos + self.height:
-                y_collision = True
-            if x_collision and y_collision:
-                # Check for head collision.
-                if player.y_pos + character_height - self.y_pos < character_height / 5:
-                    is_headjump = True
-                else:
-                    is_kill_player = True
-
-        # If the hero touches the monster (unless headjump) the hero dies.
-        if is_kill_player:
-            display_message('YOU WERE KILLED BY A HORRIBLE MONSTER!')
-        # If the hero jumped on the monsters head, it dies.
-        if is_headjump:
-            self.is_dead = True
-            self.x_speed = 0
-            # Even out the hight differences of the two pictures for alive and dead monster.
-            self.y_pos += monster_height - monster_height_dead
-
-    # Update the enemy for the next frame.
-    def update_pos(self):
-        # Update x position.
-        self.x_pos -= self.x_speed
-        # Update y position. Therefore first check falling due to gravity.
-
-    def draw(self):
-        # Find position on screen.
-        self.x_pos_on_screen = self.x_pos - camera.left_edge
-        # Draw the enemy.
-        self.blit()
-        # Check collision with player.
-        self.collision_check()
 
 
 
@@ -288,6 +333,12 @@ def game_loop():
     level.add_ground([500, display_height*.8, 100, 500])
     level.add_ground([200, display_height * 0.95, 10000, display_height * 0.05])
     level.add_ground([0,400,200,50])
+    level.add_ground([800, display_height * 0.9, 1300, display_height * 0.1])
+    level.add_ground([950, display_height * 0.85, 600, display_height * 0.15])
+    level.add_ground([2000, display_height * 0.9, 2000, display_height * 0.1])
+    level.add_ground([2100, display_height * 0.85, 1300, display_height * 0.15])
+    level.add_ground([2200, display_height * 0.75, 200, display_height * 0.25])
+    level.add_ground([5000, display_height * 0.8, 200, display_height * 0.2])
     # Initiate the player.
     x_character = (display_width * 0.45)
     y_character = (display_height * 0.8)
@@ -297,17 +348,15 @@ def game_loop():
     monster = []
     for i_monster in range(5):
         this_monster_start += random.randrange(monster_width, 1000)
-        monster.append(Enemy(this_monster_start, y_character))
+        monster.append(Enemy(this_monster_start, 0))
 
 
     # Initiate the camera.
     camera = Camera()
 
     # Initiate values
-    x_change_char = 0
-    y_change_char = 0
-
-    x_change_camera = 0
+    is_pressed_arrow_left = False
+    is_pressed_arrow_right = False
 
     # Set parameters that end the game.
     is_game_over = False
@@ -326,8 +375,10 @@ def game_loop():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     player.x_velocity_ground = -speed_run
+                    is_pressed_arrow_left = True
                 if event.key == pygame.K_RIGHT:
                     player.x_velocity_ground = speed_run
+                    is_pressed_arrow_right = True
                 if event.key == pygame.K_UP:
                     if player.is_on_ground:
                         # Player starts jumping
@@ -337,36 +388,52 @@ def game_loop():
                     # player.y_velocity = speed_jump
                     pass
             elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    is_pressed_arrow_left = False
+                if event.key == pygame.K_RIGHT:
+                    is_pressed_arrow_right = False
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     player.x_velocity_ground = 0
+                    # Check if the other arrow key is still pressed, so the character won't stop moving.
+                    if is_pressed_arrow_left:
+                        player.x_velocity_ground = -speed_run
+                    elif is_pressed_arrow_right:
+                        player.x_velocity_ground = speed_run
+
                 if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     # player.y_velocity = 0
                     pass
 
-
         # Update the player position
         player.update_pos()
+        # Update monsters.
+        for i_monster in monster:
+            # Update the new position of the monster.
+            i_monster.update_pos()
 
         # Update camera.
         camera.update()
         # Update the active ground for this frame.
         level.update_ground()
 
-        for i_monster in monster:
-            # Update the new position of the monster.
-            i_monster.update_pos()
+
+
 
         # DRAW SCREEN
         # Paint background of the display.
         Game_display.fill(white)
+        # Draw finish area.
+        pygame.draw.rect(Game_display, green, [6000 - camera.left_edge, 200, display_height*0.7, display_height*0.3])
         # Draw ground.
         level.draw_ground()
 
         pygame.draw.rect(Game_display, black, [500-camera.left_edge,110,200,60])
+
         # Draw new player position.
         player.blit()
         # Update monster positions.
-
+        for i_monster in monster:
+            i_monster.draw()
 
         # Show new frame on screen. update allows to update single parameters if given as input parameters, otherwise it
         # updates the whole screen. Alternatively you could use the flip function, which always updates the whole screen.
